7fb8ec19cce274be953a70866c6b1b505022f936
Sahil Bhatti <sahilbhatti9624@gmail.com>
2026-06-18 14:43:42 +0530

    Optimize hero entrance animations

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -194,7 +194,7 @@ pb-10
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{
-            duration: 1,
+            duration: 0.4,
           }}
         >
 
@@ -202,9 +202,8 @@ pb-10
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{
-              delay: 0.2,
-              duration: 1,
+           transition={{
+            duration: 0.3,
             }}
             className="
               flex
